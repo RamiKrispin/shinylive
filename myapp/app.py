6ec6e11a71544c3b9ab1360a407673cd6bd12dc9
RamiKrispin <ramkrisp@umich.edu>
2022-08-05 16:39:13 -0700

    update the app readme

--- myapp/app.py
+++ myapp/app.py
@@ -56,12 +56,15 @@ app_ui = ui.page_fluid(
 
         Chapter 3 of [Statistical Rethinking's](https://xcelab.net/rm/statistical-rethinking/) by Prop. Richard McElreath focuses on normal distribution
         and its characteristics. It illustrates how to generate a normal distribution using the soccer field experiment:
-            - Place a bunch of people at the center line of a soccer field
-            - Each person flips a coin and moves one step to the right or left according to the outcome (head or tail)
-            - Repeat this process multiple times
+
+        - Place a bunch of people at the center line of a soccer field
+        - Each person flips a coin and moves one step to the right or left according to the outcome (head or tail)
+        - Repeat this process multiple times
         After a couple of iterations, you will notice the distribution of the people's distances across the field will become Gaussian or normal (e.g., bell-curved shape).
 
         The app above simulates this experience by setting the sample size (i.e., number of people) and number of iterations. Where on each iteration, we draw a random number between -1 and 1 (can choose between float integer steps with the `Step Type` drop-down). The plot above shows the cumulative sum of each experiment across each step of the experience. You can notice how the distribution becomes more Gaussin as the number of steps increases.
+
+        Code available [here](https://github.com/RamiKrispin/shinyelive).
 
         """
     ),
