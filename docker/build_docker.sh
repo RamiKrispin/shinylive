ecd45604b5ec1e9a279f547a3da73419d70e2ad2
RamiKrispin <ramkrisp@umich.edu>
2022-10-04 23:07:20 -0700

    add init docker

---
+++ docker/build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Build the docker"
+
+docker build . --progress=plain \
+               --build-arg CONDA_ENV=shinylive \
+               --build-arg QUARTO_VERSION=1.1.149 \
+               --build-arg PYTHON_VER=3.10 \
+               -t rkrispin/shinylive:dev.0.0.0.9000
+
+if [[ $? = 0 ]] ; then
+echo "Pushing docker..."
+docker push rkrispin/shinylive:dev.0.0.0.9000
+else
+echo "Docker build failed"
+fi
---
+++ docker/install_dependencies.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#Environment variables
+QUARTO_VERSION=$1 
+CONDA_ENV=$2 
+PYTHON_VER=$3
+
+# Install dependencies
+apt-get update && apt-get install -y --no-install-recommends \
+    jq \
+    libxml2-dev \
+    zlib1g \
+    g++-11 \
+    libz-dev \
+    freetype2-demos \
+    libpng-dev \
+    libtiff-dev \
+    libjpeg-dev \
+    make \
+    fontconfig \
+    libfribidi-dev \
+    libharfbuzz-dev \
+    libfontconfig1-dev \
+    git \
+    vim \
+    curl \
+    sudo \
+    wget \
+    && rm -rf /var/lib/apt/lists/*
+
+#Italling Quarto
+QUARTO_VERSION=$1
+TEMP_QUARTO="$(mktemp)" &&
+wget --no-check-certificate -O "$TEMP_QUARTO" https://github.com/quarto-dev/quarto-cli/releases/download/v$QUARTO_VERSION/quarto-${QUARTO_VERSION}-linux-amd64.deb &&
+sudo dpkg -i "$TEMP_QUARTO"
+rm -f "$TEMP_QUARTO"
+
+# Install miniconda
+sudo apt update && apt-get install -y --no-install-recommends \
+    software-properties-common \
+    && sudo add-apt-repository -y ppa:deadsnakes/ppa \
+    && sudo apt update 
+
+wget --no-check-certificate https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh \
+    && /bin/bash ~/miniconda.sh -b -p /opt/conda \
+    && export PATH=/opt/conda/bin:$PATH \
+    && conda init bash \
+    && conda install conda-build
+
+. /root/.bashrc \
+    && conda create -y --name $CONDA_ENV python=$PYTHON_VER 
+
+echo "conda activate $CONDA_ENV" >> ~/.bashrc
+
+#. /root/.bashrc && conda activate $CONDA_ENV && conda info --envs && pip install package_name == package_version
